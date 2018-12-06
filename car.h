1de5ca92e0afcfbd85fe3ca00981ce6e1a63e33d
LucaTomala <43435523+LucaTomala@users.noreply.github.com>
2018-12-06 13:03:36 +0100

    Update car.h

--- car.h
+++ car.h
@@ -12,5 +12,19 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+enum Type { AIXAM, FIAT_MULTIPLA, JEEP };
+enum Color { RED, GREEN, BLACK, BLUE, ORANGE, SILVER };
+
+typedef struct CarImplementation* Car;
+
+Car get_car(enum Type type);
+void init();
+void accelerate(Car car);
+void set_acceleration_rate(Car car, int rate);
+int get_speed(Car car);
+enum Color get_color(Car car);
+double get_fill_level(Car car);
+enum Type get_type(Car car);
+double get_acceleration_rate(Car car);
 
 #endif
